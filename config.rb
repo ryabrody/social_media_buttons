6b9cd42092915394e45ef63c77e6f38dd21e17f3
Franca Rast <franca.rast@local.ch>
2016-01-27 11:54:10 +0100

    add social media examples

--- config.rb
+++ config.rb
@@ -23,11 +23,33 @@ page '/*.txt', layout: false
 ###
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def share_on_google_plus_url(share_url)
+    "https://plus.google.com/share?url=#{share_url}"
+  end
+
+  def share_on_google_plus_link(share_url)
+    link_to 'Share on Google+', share_on_google_plus_url(share_url)
+  end
+
+  def share_on_facebook_url(share_url)
+    "https://www.facebook.com/sharer/sharer.php?u=#{share_url}"
+  end
+
+  def share_on_facebook_link(share_url)
+    link_to 'Share on Facebook', share_on_facebook_url('http://www.google.ch')
+  end
+
+  def share_on_twitter_url(share_url)
+    "https://twitter.com/home?status=#{share_url}"
+  end
+
+  def share_on_twitter_link(share_url)
+    link_to 'Share on Twitter', "https://twitter.com/home?status=#{share_url}"
+  end
+
+
+end
 
 # Build-specific configuration
 configure :build do
